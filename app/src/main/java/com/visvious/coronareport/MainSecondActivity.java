07c11add12f00d494df6f98fd71423ad908baea7
Visuddha Karaunratne <vkarunarthne@virtusa.com>
2020-03-21 21:58:09 +0530

    first commit

---
+++ app/src/main/java/com/visvious/coronareport/MainSecondActivity.java
@@ -0,0 +1,14 @@
+package com.visvious.coronareport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainSecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+    }
+}
