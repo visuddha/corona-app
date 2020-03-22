6b7179c5e8938ff60c8d9eb55ecd673ff6099f5f
Visuddha Karaunratne <vkarunarthne@virtusa.com>
2020-03-22 12:14:06 +0530

    intial release 1.0.0

--- app/src/androidTest/java/com/visvious/coronareport/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/visvious/coronareport/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.visvious.coronareport", appContext.getPackageName());
+        assertEquals("com.visvious.com.visuvious.coronareport", appContext.getPackageName());
     }
 }
